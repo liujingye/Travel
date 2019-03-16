28df9f93a550820b2362d6f91104d29f2d70fb4c
liujingye <1286518245@qq.com>
2019-03-16 16:37:19 +0800

    city-ajax

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,20 +1,17 @@
 <template>
 	<div>
 		<ul class="list">
-			<li class="item">A</li>
-			<li class="item">A</li>
-			<li class="item">A</li>
-			<li class="item">A</li>
-			<li class="item">A</li>
-			<li class="item">A</li>
-			<li class="item">A</li>
+			<li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
 		</ul>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: 'CityAlphabet'
+		name: 'CityAlphabet',
+		props: {
+			cities:Object
+		}
 	}
 </script>
 
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -9,61 +9,32 @@
 					</div>
 				</div>
 			</div>
-				<div class="area">
-					<div class="title border-topbottom">热门城市</div>
-					<div class="button-list">
-						<div class="button-wrapper">
-							<div class="button">北京</div>
-						</div>
-						<div class="button-wrapper">
-							<div class="button">北京</div>
-						</div>
-						<div class="button-wrapper">
-							<div class="button">北京</div>
-						</div>
-						<div class="button-wrapper">
-							<div class="button">北京</div>
-						</div>
-						<div class="button-wrapper">
-							<div class="button">北京</div>
-						</div>
-					</div>
-				</div>
-				<div class="area">
-					<div class="title border-topbottom">A</div>
-					<div class="item-list">
-						<div class="item border-bottom">阿拉尔</div>
-						<div class="item border-bottom">阿拉尔</div>
-						<div class="item border-bottom">阿拉尔</div>
-						<div class="item border-bottom">阿拉尔</div>
-					</div>
-				</div>
-				<div class="area">
-					<div class="title border-topbottom">A</div>
-					<div class="item-list">
-						<div class="item border-bottom">阿拉尔</div>
-						<div class="item border-bottom">阿拉尔</div>
-						<div class="item border-bottom">阿拉尔</div>
-						<div class="item border-bottom">阿拉尔</div>
-					</div>
-				</div>
-				<div class="area">
-					<div class="title border-topbottom">A</div>
-					<div class="item-list">
-						<div class="item border-bottom">阿拉尔</div>
-						<div class="item border-bottom">阿拉尔</div>
-						<div class="item border-bottom">阿拉尔</div>
-						<div class="item border-bottom">阿拉尔</div>
+			<div class="area">
+				<div class="title border-topbottom">热门城市</div>
+				<div class="button-list">
+					<div class="button-wrapper" v-for="item of hot" :key="item.id">
+						<div class="button">{{item.name}}</div>
 					</div>
 				</div>
 			</div>
+			<div class="area" v-for="(item, key) of cities" :key="key">
+				<div class="title border-topbottom">{{key}}</div>
+				<div class="item-list">
+					<div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
+				</div>
+			</div>
 	</div>
+</div>
 </template>
 
 <script>
 	import Bscroll from 'better-scroll'
 	export default {
 		name: 'CityList',
+		props: {
+			hot: Array,
+			cities: Object
+		},
 		mounted () {
 			this.scroll = new Bscroll(this.$refs.wrapper)
 		}
@@ -106,7 +77,6 @@
 					border: .02rem solid #ccc
 					border-radius: .06rem
 	.item-list
-		
 		.item
 			line-height: .76rem
 			padding-left: .2rem
